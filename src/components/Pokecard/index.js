0bbc47701a896eaa17e9060892b6cf2bfce54bab
juancarlos <juancarlosenrique103@gmail.com>
2021-02-26 16:33:29 -0500

    Add styles and created component Pokecard :zap:

--- src/components/Pokecard/index.js
+++ src/components/Pokecard/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./style.css";
 
 const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
@@ -7,7 +8,7 @@ function Pokecard({id, name, type, exp}) {
   return (
     <div className="Pokecard">
       <h1>{name}</h1>
-      <img src={imgSrc}></img>
+      <img src={imgSrc} alt={name}></img>
       <div>Type: {type}</div>
       <div>Experience: {exp}</div>
     </div>
