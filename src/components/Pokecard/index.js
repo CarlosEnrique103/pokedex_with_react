169bd3ec47f0cacb081e4d078269f1bded14848f
juancarlos <juancarlosenrique103@gmail.com>
2021-02-26 16:54:03 -0500

    Add fancier images to component Pokecard :zap:

--- src/components/Pokecard/index.js
+++ src/components/Pokecard/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
 import "./style.css";
 
-const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+//const POKE_API = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API_BETTER =
+  "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
-function Pokecard({id, name, type, exp}) {
-  const imgSrc= `${POKE_API}${id}.png`;
+function addZeros(num) {
+  const newNum = `${"0".repeat(3 - String(num).length)}${num}`;
+  return newNum;
+  // switch (String(num).length) {
+  //   case 1:
+  //     return `00${num}`
+  //   case 2:
+  //     return `0${num}`
+  //   default:
+  //     return String(num);
+  // }
+}
+
+function Pokecard({ id, name, type, exp }) {
+  const idx = addZeros(id);
+  const imgSrc = `${POKE_API_BETTER}${idx}.png`;
   return (
     <div className="Pokecard">
       <h1>{name}</h1>
@@ -12,7 +28,7 @@ function Pokecard({id, name, type, exp}) {
       <div>Type: {type}</div>
       <div>Experience: {exp}</div>
     </div>
-  )
+  );
 }
 
-export default Pokecard
+export default Pokecard;
